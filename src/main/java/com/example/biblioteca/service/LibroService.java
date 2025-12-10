b7768e06d09753023a154034d1ddd4266755b1bc
moises <devilmoicry@gmail.com>
2025-12-10 12:36:30 +0000

    injected repository to libroService

--- src/main/java/com/example/biblioteca/service/LibroService.java
+++ src/main/java/com/example/biblioteca/service/LibroService.java
@@ -1,7 +1,17 @@
 package com.example.biblioteca.service;
 
+import com.example.biblioteca.repository.LibroRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class LibroService {
+
+    private final LibroRepository repo;
+
+    @Autowired
+    public LibroService(final LibroRepository repo){
+        this.repo= repo;
+    }
+
 }
