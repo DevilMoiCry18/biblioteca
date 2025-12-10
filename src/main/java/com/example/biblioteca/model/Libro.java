6199de57e38b0b0ee6b7dc27ad42be788ac43347
moises <devilmoicry@gmail.com>
2025-12-10 13:27:22 +0000

    Simplified all, Corrected Libro, corrected LibroController, added data.sql file

--- src/main/java/com/example/biblioteca/model/Libro.java
+++ src/main/java/com/example/biblioteca/model/Libro.java
@@ -1,5 +1,6 @@
 package com.example.biblioteca.model;
 
+import com.example.biblioteca.service.LibroService;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -17,4 +18,32 @@ public class Libro {
     private String isbn;
     private Date fechaPublicacion;
     private String genero;
+
+    public Libro(long id, String titulo, String isbn, Date fechaPublicacion, String genero) {
+        this.id = id;
+        this.titulo = titulo;
+        this.isbn = isbn;
+        this.fechaPublicacion = fechaPublicacion;
+        this.genero = genero;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public Date getFechaPublicacion() {
+        return fechaPublicacion;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
 }
